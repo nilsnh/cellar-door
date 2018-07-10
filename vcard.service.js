49ca2786473dbc2319e5401099da5fcb205e498c
Nils Norman Haukås <nils@thunki.com>
2018-07-10 21:25:48 +0200

    add vcard service with test

---
+++ vcard.service.js
@@ -0,0 +1,32 @@
+const microFormat = require('microformat-node')
+const rp = require('require-promise-native')
+
+module.exports = async url => {
+  try {
+    const html = await rp(url)
+    const { items } = microFormat.get({ html })
+    const vcard = items.find(({ type = [] }) =>
+      type.find(typeName => typeName === 'h-card')
+    )
+    if (!vcard) {
+      return null
+    }
+    return unwrapProperties(vcard.properties)
+  } catch (e) {
+    console.error('Failed to fetch vcard data', e)
+    return null
+  }
+}
+
+// for each property that is an array, pick the first array value as replacement.
+function unwrapProperties(obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    const propertyValue = obj[key]
+    if (Array.isArray(propertyValue)) {
+      acc[key] = propertyValue[0] || ''
+    } else {
+      acc[key] = propertyValue
+    }
+    return acc
+  }, {})
+}
---
+++ vcard.service.test.js
@@ -0,0 +1,33 @@
+const test = require('ava')
+const mock = require('mock-require')
+
+const user = {
+  photo: 'http://localhost:1313/me_lw.png',
+  email: 'mail@nilsnh.no',
+  note: 'Keep the web weird and wonderful.',
+  name: 'Nils Norman Haukås',
+  url: 'http://localhost:1313/'
+}
+
+// the vcard service will try to call a remote url. We mock that request and
+// return this html instead to be parsed by microformat-node.
+mock(
+  'require-promise-native',
+  () => `<html>
+  <a class="h-card" href="${user.url}">
+    <img class="u-photo" src="${
+      user.photo
+    }" width="60" height="60" alt="nilsnh.no">
+    <span rel="me" class="p-name" style="display:none;">Nils Norman Haukås</span>
+    <span rel="me" class="u-email" style="display:none;">${user.email}</span>
+    <span rel="me" class="u-note" style="display:none;">${user.note}</span>
+  </a>
+</html>`
+)
+
+const vcardService = require('./vcard.service')
+
+test('test that we can correctly extract vcard information', async t => {
+  const vcardData = await vcardService()
+  t.deepEqual(vcardData, user, 'was not able to extract correct vcard data')
+})
